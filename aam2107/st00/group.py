0bf776abef305a2ab50e9b70dc531ca31e7d8ac3
dl <dl@bugtraq.ru>
2021-10-08 13:27:13 +0300

    (!) initial commit

---
+++ aam2107/st00/group.py
@@ -0,0 +1,3 @@
+﻿class group:
+	def f(self):
+		return "aam2107.st00.group.f()"
---
+++ lab2.py
@@ -0,0 +1,37 @@
+﻿import os
+import threading
+import time
+
+MENU = [
+    ["[2104-00] Образец 2104", "asm2104/st00/main.py"],
+    ["[2105-00] Образец 2105", "asm2105/st00/main.py"],
+    ["[2107-00] Образец 2107", "aam2107/st00/main.py"],
+    #		добавить пункт меню для вызова своей главной функции по шаблону:
+    #		["[<код группы>-<номер по журналу>] <Фамилия>", "<путь до модуля>"],
+]
+
+
+def launcher():
+    time.sleep(3)
+    os.system("start http://localhost:5000/")
+
+
+def menu():
+    print("------------------------------")
+    for i, item in enumerate(sorted(MENU)):
+        print("{0:2}. {1}".format(i, item[0]))
+    print("------------------------------")
+    return int(input())
+
+
+try:
+    while True:
+        try:
+            app = sorted(MENU)[menu()][1]
+            threading.Thread(target=launcher).start()
+            if os.system("python3 " + app):
+                os.system("python " + app)
+        except KeyboardInterrupt:
+            pass
+except Exception as ex:
+    print(ex, "\nbye")
