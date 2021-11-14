ed65b2723e5978fcd6d7218568006f417d2e0f21
era111 <dmitrieva.era@yandex.ru>
2021-11-14 20:54:08 +0300

    Дмитриева Ирина ЛР2

---
+++ asm2105/st05/Captain.py
@@ -0,0 +1,10 @@
+from .Player import Player
+
+
+class Captain(Player):
+    def __init__(self, name='', surname='', age='', exp=''):
+        super().__init__(name, surname, age)
+        self.experience = exp
+
+    def __str__(self):
+        return super().__str__() + f' Опыт { self.experience }'
