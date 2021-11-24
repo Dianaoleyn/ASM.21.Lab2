60af5fb06374219187004c6a4da04065617ba1be
anfijk <91594026+anfijk@users.noreply.github.com>
2021-11-24 10:51:19 +0300

    Ushcheko Anfisa

---
+++ aam2107/st15/Captain.py
@@ -0,0 +1,12 @@
+from Student import Student
+
+
+
+class Captain(Student):
+    def __init__(self, name='', surname='', age='', grants=''):
+        super().__init__(name, surname, age)
+        self.__grants = grants
+
+    def __str__(self):
+        return super().__str__() + f', Надбавка { self.__grants }'
+
